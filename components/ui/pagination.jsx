0d792eae1ef77ae1ac70253e65c63f06fa1c2741
Kourdroid <kourdroid@gmail.com>
2024-01-08 23:39:29 +0100

    Update pagination.jsx

--- components/ui/pagination.jsx
+++ components/ui/pagination.jsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/Button";
 
 const Pagination = ({
   className,
